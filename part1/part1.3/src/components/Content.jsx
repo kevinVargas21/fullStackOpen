6a39297c1943e781274095bbaff8f31c6ab91664
kevinVargas21 <kevinVargas21@users.noreply.github.com>
2023-05-02 15:17:06 -0600

    part3

---
+++ part1/part1.3/src/components/Content.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Part1 from './Part1';
+import Part2 from './Part2';
+import Part3 from './Part3';
+
+const Content = () => {
+
+  return (
+    <div>
+      <Part1/>
+      <Part2/>
+      <Part3/>
+    </div>
+  )
+};
+
+export default Content;
---
+++ part1/part1.3/src/components/Part3.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Part3 = (props) => {
+    const part3 = 'State of a component'
+    const exercises3 = 14
+
+    return (
+        <div>
+            <p>
+                {part3} {exercises3}
+            </p>
+        </div>
+    )
+};
+
+export default Part3;

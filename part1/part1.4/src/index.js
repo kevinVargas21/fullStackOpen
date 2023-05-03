4bdf2ffd41f63da348024691d8acfec9c5f74567
kevinVargas21 <82131754+kevinVargas21@users.noreply.github.com>
2023-05-03 12:00:38 -0600

    part1.4

--- part1/part1.4/src/index.js
+++ part1/part1.4/src/index.js
@@ -39,7 +39,7 @@ const App = () => {
       </p>
 
       <p>
-        Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+        Total of exercises are {parts[0].exercises + parts[1].exercises + parts[2].exercises}
       </p>
 
     </div>

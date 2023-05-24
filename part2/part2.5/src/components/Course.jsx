bfe3974404c32d51e2b6e430d81eac17a854cb47
kevinVargas21 <82131754+kevinVargas21@users.noreply.github.com>
2023-05-24 16:42:05 -0600

    part2.4

--- part2/part2.5/src/components/Course.jsx
+++ part2/part2.5/src/components/Course.jsx
@@ -16,13 +16,16 @@ const Course = ({ courses }) => {
 
             {courses.map((course) => (
                 <div key={course.id}>
-                    <h2>{course.name}</h2>
+                    <h2> {course.name}</h2>
 
-                    {course.parts.map((part) => (                       
+                    {course.parts.map((part) => (
+                        <>
                             <p key={part.id}>
                                 {part.name} {part.exercises}
                             </p>
-         
+
+                        </>
+
                     ))}
                     <h3>Total of {totalExercises(course)} exercises </h3>
                 </div>
